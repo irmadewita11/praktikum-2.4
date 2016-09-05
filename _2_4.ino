095d3ed801cb6090143cbaa921cf839e3331d8bd
irmadewita11 <irmadewita123@gmail.com>
2016-09-05 16:20:43 +0700

    Add files via upload

---
+++ _2_4.ino
@@ -0,0 +1,13 @@
+void setup(){
+  pinMode(A0, INPUT);
+  digitalWrite(A0, HIGH);
+  pinMode(12, OUTPUT);
+}
+void loop(){
+ if(digitalRead(A0)==LOW){
+     digitalWrite(12, HIGH);
+ }
+ else{  
+  digitalWrite(12, LOW);
+}
+}
